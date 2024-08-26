f784f2a11146d7cc1a29d1653117c7831732055b
stacypere <stacy.pere@strathmore.edu>
2024-08-26 10:14:22 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Title: Bio Data")
+st.write("This is a sample web app")
+
+first_name = st.text_input("First Name")
+last_name = st.text_input("Last Name")
+gender = st.selectbox("Gender", ["Male", "Female"])
+age = st.number_input("Your age", 0, 100, 30, 1)
+dob = st.date_input("Your birthday")
+marital_status = st.radio("Marital status", ["Single", "Married"])
+years_of_experience = st.slider("Years of experience", 0, 40)
